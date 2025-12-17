d225298a3e8172c840ce9ec68c020255600115d9
ilkertstn <ilkertstn@gmail.com>
2025-12-17 03:44:19 +0300

    bug console

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {};
-
-export default nextConfig;
+if (process.env.VERCEL) {
+  console.log("ENV CHECK (build):", {
+    apiKey: !!process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: !!process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: !!process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    appId: !!process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    messagingSenderId: !!process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    storageBucket: !!process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    databaseURL: !!process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  });
+}
