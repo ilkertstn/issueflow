9831805a01f1686df83a76c4509b41370938159d
ilkertstn <ilkertstn@gmail.com>
2025-12-17 03:40:23 +0300

    bugfix

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import type { NextConfig } from "next";
 
-if (process.env.VERCEL) {
-  console.log("ENV CHECK (build):", {
-    hasApiKey: !!process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    hasProjectId: !!process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    hasAppId: !!process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  });
-}
+const nextConfig: NextConfig = {};
 
-module.exports = nextConfig;
+export default nextConfig;
